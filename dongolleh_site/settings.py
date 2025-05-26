37bf5aeb28c63101a2b261f32fefd83e6147b00f
Gaga-Marda-Bur <gagabourbour@gmail.com>
2025-05-26 09:54:30 +0300

    settings.py

--- dongolleh_site/settings.py
+++ dongolleh_site/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'contacts.User'
+AUTH_USER_MODEL = 'dongolleh_site.User'
 LOGIN_URL = '/admin'
 
 STORAGES = {
